be19c17dc13c9de9d64179bb27a6f50dcb9ca2a8
kizmer <kamil.izmer@relayr.io>
2018-11-06 21:17:04 +0100

    Adden element with simple movement

--- games/FallingBalls/FallingBalls.py
+++ games/FallingBalls/FallingBalls.py
@@ -1,9 +1,14 @@
 import pyjd
+from pyjamas import DOM
 from pyjamas.Canvas.GWTCanvas import GWTCanvas
 from pyjamas.ui.FocusPanel import FocusPanel
-from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.RootPanel import RootPanel, RootPanelCls
 from pyjamas.Canvas import Color
 from pyjamas.Canvas.ImageLoader import loadImages
+from pyjamas.ui.ClickListener import ClickHandler
+from pyjamas.ui.KeyboardListener import KeyboardHandler
+from pyjamas.ui import Event
+from pyjamas.Timer import Timer
 
 class GameCanvas(GWTCanvas):
     def __init__(self, width, height):
@@ -11,55 +16,119 @@ class GameCanvas(GWTCanvas):
         self.width = width
         self.height = height
         self.background = ''
-        # self.ball = ''
-        # self.game = Game(width, height)
-        # self.controller = Controller(self.game)
-        self.resize(self.width, self.height)
-        self.fillRect(0,0,self.width,self.height)
-        images = ['./images/background.bmp']#,
-                    #'./images/ball.bmp']
+        self.ball = ''
+        self.game = Game(width, height)
+        self.controller = Controller(self.game)
+        # self.resize(self.width, self.height)
+        # self.fillRect(0,0,self.width,self.height)       
+        images = ['./images/background.png',
+                    './images/ball.png']
         loadImages(images, self)
+        self.sinkEvents(Event.KEYEVENTS)
 
     def onImagesLoaded(self, imagesHandles):
         self.background = imagesHandles[0]
-        # self.ball = imagesHandles[1]
+        self.ball = imagesHandles[1]
         self.resize(self.width, self.height)
         self.drawImage(self.background, 0, 0)
-        # self.controller.startGame(self)
+        self.controller.startGame(self)
+        
+    def addTo(self, panel):
+        panel.add(self)
+        self.top = DOM.getAbsoluteTop(self.getElement())
+        self.left = DOM.getAbsoluteLeft(self.getElement())
 
-#     def drawBall(self, ball):
-#         self.translate(ball.x, ball.y)
-#         self.drawImage(self.ball)
+    def setKey(self, k, set):
+        DOM.eventPreventDefault(DOM.eventGetCurrentEvent())
+        if k == 39:
+            self.controller.keyLeft = set
+        elif k == 37:
+            self.controller.keyRight = set
 
-# class Ball:
-#     def __init__(self, x, y):
-#         self.x=x
-#         self.y=x
+    def onKeyDown(self, sender, keyCode, modifiers = None):
+        self.setKey(keyCode, True)
 
-#     def startGame(self, game):
-#         self.game = game
-#         self.game.start_game(game)
+    def onKeyUp(self, sender, keyCode, modifiers = None):
+        self.setKey(keyCode, False)
 
-# class Game:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#         self.ball = Ball(250,200)
-#         self.game =''
+    def drawBall(self, ball):
+        self.saveContext()
+        self.translate(ball.x, ball.y)
+        self.drawImage(self.ball, 0, 0)
+        self.restoreContext()
 
-#     def startGame(self, game):
-#         self.game = game
+    def drawElements(self):
+        self.fillRect(0,0,self.width,self.height)
+        self.drawImage(self.background, 0, 0)
+        self.drawBall(self.game.ball)
 
-#     def update(self):
-#         self.game.drawBall(self.ball)
+class Ball:
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+        self.dx=0
+        self.speed=10
 
-# class Controller:
-#     def __init__(self, game):
-#         self.game=game
+    def moveLeft(self):
+            self.dx=self.speed
 
-#     def startGame(self, game):
-#         self.game = game
-#         self.game.startGame(game)  
+    def moveRight(self):
+            self.dx=-self.speed
+
+    def move(self):
+        self.x+=self.dx
+        if self.x < 0 or self.x > 470:
+            self.dx=-self.dx
+
+class Game:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ball = Ball(235,322)
+        self.canvas = ''
+
+    def startGame(self, canvas):
+        self.canvas = canvas
+
+    def update(self):
+        self.ball.move()
+        self.canvas.drawElements()
+
+class Controller:
+    def __init__(self, game):
+        self.game=game
+        self.keyLeft = False
+        self.keyRight = False
+
+    def startGame(self, canvas):
+        self.canvas = canvas
+        self.game.startGame(canvas)  
+        self.timer = Timer(notify=self.update)
+        self.timer.scheduleRepeating(35)
+
+    def update(self):
+        self.pressedKey()
+        self.game.update()
+
+    def pressedKey(self):
+        ball = self.game.ball
+        if self.keyLeft:
+            ball.moveLeft()
+        if self.keyRight:
+            ball.moveRight()
+
+class RootPanelListener(RootPanelCls, KeyboardHandler, ClickHandler):
+    def __init__(self, Parent, *args, **kwargs):
+        self.Parent = Parent
+        self.focussed = False
+        RootPanelCls.__init__(self, *args, **kwargs)
+        ClickHandler.__init__(self)
+        KeyboardHandler.__init__(self)
+        self.addClickListener(self)
+
+    def onClick(self, Sender):
+        self.focussed = not self.focussed
+        self.Parent.setFocus(self.focussed)
 
 if __name__ == '__main__':
     pyjd.setup("public/FallingBalls.html")
@@ -68,5 +137,4 @@ if __name__ == '__main__':
     RootPanel().add(panel)
     panel.addKeyboardListener(c)
     panel.setFocus(True)
-
-pyjd.run()
+    pyjd.run()
