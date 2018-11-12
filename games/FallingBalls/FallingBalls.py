e031b8548a36688e1277d8c4e067f936a4909de9
kizmer <kamil.izmer@relayr.io>
2018-11-12 17:45:17 +0100

    Added collision and scoring

--- games/FallingBalls/FallingBalls.py
+++ games/FallingBalls/FallingBalls.py
@@ -9,19 +9,22 @@ from pyjamas.ui.ClickListener import ClickHandler
 from pyjamas.ui.KeyboardListener import KeyboardHandler
 from pyjamas.ui import Event
 from pyjamas.Timer import Timer
+from pyjamas.ui.Label import Label
+from pyjamas.ui.VerticalPanel import VerticalPanel
 import random
+import math
 
 class GameCanvas(GWTCanvas):
-    def __init__(self, width, height):
+    def __init__(self, width, height, label, label1):
         GWTCanvas.__init__(self, width, height)
         self.width = width
         self.height = height
+        self.label = label
+        self.label1 = label1
         self.background = ''
         self.ball = ''
         self.game = Game(width, height)
-        self.controller = Controller(self.game)
-        # self.resize(self.width, self.height)
-        # self.fillRect(0,0,self.width,self.height)       
+        self.controller = Controller(self.game)      
         images = ['./images/background.png',
                     './images/ball.png',
                     './images/ball2.png']
@@ -95,7 +98,7 @@ class Opponent:
     def __init__(self, ball):
         self.x=random.randint(0,470)
         self.y=-15
-        self.speed=random.randint(10,20)
+        self.speed=random.randint(15,25)
         self.ball=ball
 
     def move(self):
@@ -109,6 +112,8 @@ class Game:
         self.canvas = ''
         self.counter=0
         self.opponents=[]
+        self.points=0
+        self.pointsBest=0
 
     def startGame(self, canvas):
         self.canvas = canvas
@@ -117,14 +122,29 @@ class Game:
         if self.counter == 10:
             self.opponents.append(Opponent(self.ball))
             self.counter=0
+            self.points+=1
+            self.canvas.label.setText("Current score: " + str(self.points))
         else:
             self.counter+=1
         for o in self.opponents:
             o.move()
+            if (distance(self.ball.x+15, self.ball.y+15, o.x+15, o.y+15)<(50)):
+                self.resetGame()
             if o.y > 420:
                 self.opponents.remove(o) 
         self.ball.move()
         self.canvas.drawElements()
+
+    def resetGame(self):
+        self.ball.x=235
+        self.ball.y=322
+        self.ball.dx=0
+        if self.points>self.pointsBest:
+            self.pointsBest=self.points
+        self.points=0
+        self.canvas.label.setText("Current score: 0")
+        self.canvas.label1.setText("Best score: " + str(self.pointsBest))
+        self.opponents=[]
 
 class Controller:
     def __init__(self, game):
@@ -162,11 +182,20 @@ class RootPanelListener(RootPanelCls, KeyboardHandler, ClickHandler):
         self.focussed = not self.focussed
         self.Parent.setFocus(self.focussed)
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))
+
 if __name__ == '__main__':
     pyjd.setup("public/FallingBalls.html")
-    c = GameCanvas(500, 400)
+    l = Label("Current score: 0")
+    l1 = Label("Best score: 0")
+    c = GameCanvas(500, 400, l, l1)
     panel = FocusPanel(Widget=c)
+    panel1 = VerticalPanel()
+    panel1.add(l)
+    panel1.add(l1)
     RootPanel().add(panel)
+    RootPanel().add(panel1)
     panel.addKeyboardListener(c)
     panel.setFocus(True)
     pyjd.run()
